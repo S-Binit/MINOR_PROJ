65a0ee8e8bf4fdaf53226ffe29a08e0a9609ca76
Binit Shrestha <bnitshrestha078@gmail.com>
2025-06-14 12:21:20 +0545

    CreatedFirstIndexPage

--- app/index.jsx
+++ app/index.jsx
@@ -1,30 +1,16 @@
-import { StyleSheet, Text, View, Image} from 'react-native' //imports core components for React Native
-import React from 'react'  //imports react so we can use jsx and components\
-import Logo from '../assets/logo1.png' //need to import logo as it is not inbuilt in ReactNative
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-const Home = () => {                  //simply a function "Home"
-  return (                            
-    <View style ={styles.container}>    {/*view is a container, we pulled container object from styless in <view style>*/}
-      <Image source={Logo}></Image>
-
-      <Text style = {styles.title}>The Number 1</Text>  {/*we pulled title object from styless in <view style>*/}
-
-      <Text style = {{marginTop: 10, marginBottom: 30}}>Reading List App</Text>  {/*we can also do inline styles*/}
-
-    </View>                                    
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
   )
 }
 
-export default Home             //exports a function so it can be used elsewhere.
+export default Home
 
-const styles = StyleSheet.create({            //setting up a styles object
-  container: {                                 //container is an object itself
-    flex: 1,                                  
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 18
-  }
-})      
+const styles = StyleSheet.create({
+    
+})
