7b750f2c5acbd0b30fed6497b788e2b8f1413bb2
Binit Shrestha <bnitshrestha078@gmail.com>
2025-06-14 12:54:56 +0545

    addedstyles_logo

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image} from 'react-native'
 import React from 'react'
+import Logo from '../assets/logo1.png' 
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Image source={Logo}></Image>
+
+        <Text style={[styles.title, {color: 'purple'}]}>The Number 1</Text>
+
+        <Text style={{marginTop: 10, marginBottom: 30}}>Waste Tracking App</Text>
+
     </View>
+    
   )
 }
 
 export default Home
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+  
 })
